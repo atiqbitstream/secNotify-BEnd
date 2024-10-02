d51b0f82db1ae19dadf63fd9fff9ab2603989684
atiqbitstream <atiq.bitstream@gmail.com>
2024-10-02 16:04:44 +0500

    minors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import "reflect-metadata"
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot({
+        isGlobal:true,
+        envFilePath:".local.env",
+        // envFilePath:".prod.env",
+        
+      })],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        synchronize: true,
+        entities :[__dirname+'/**/*.entity{.ts,.js}']
+      }),
+      inject: [ConfigService],
+    }),
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
