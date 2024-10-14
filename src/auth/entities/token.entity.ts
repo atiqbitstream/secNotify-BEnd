55695b7529aaca655244496998f916592beee185
atiqbitstream <atiqbitstream@gmail.com>
2024-10-14 17:24:02 +0500

    building relations

---
+++ src/auth/entities/token.entity.ts
@@ -0,0 +1,28 @@
+import { User } from "src/users/entities/user.entity";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToOne } from "typeorm"
+import { Account } from "./account.entity";
+
+@Entity()
+export class Token {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    token: string
+
+    @Column()
+    expirationTime: Date
+
+    @Column()
+    tokenType:string;
+
+    @Column()
+    sessionId:string;
+
+   
+    @ManyToOne(()=>User,(user)=>user.tokens)
+     user:User;
+
+    
+
+}
