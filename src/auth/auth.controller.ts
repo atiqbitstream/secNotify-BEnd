0a1b1d04fe74a95603d0a65d85523b0770b2ed5d
atiqbitstream <atiqbitstream@gmail.com>
2024-11-11 20:05:07 +0500

    integrating 'swagger' inside the project

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,11 +2,12 @@ import { UsersService } from 'src/users/users.service';
 import { Body, Controller, Post, Req, Res, UseGuards } from "@nestjs/common";
 import { AuthService } from "./services/auth.service";
  import { Request  } from '@nestjs/common';
- import { Response } from 'express';
+ import { response, Response } from 'express';
 
 import { CreateUserDto } from "src/users/dto/create-user.dto";
 import { TokenService } from './services/token.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { HttpService } from '@nestjs/axios';
 
 
 @Controller('auth')
@@ -14,10 +15,9 @@ export class AuthController {
   constructor( private authService:AuthService,private userService:UsersService, private tokenService:TokenService) {}
 
 
-
-
   @Post('login')
   async login(@Body() createUserDto: CreateUserDto, @Req() { }: Request) {
+
     return  this.authService.login(createUserDto);
 
   }
